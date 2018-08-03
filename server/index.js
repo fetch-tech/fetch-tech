508891c90466030a876c14d1d557ac649b02c839
gv4383 <gregory.vargas18@gmail.com>
2018-08-03 14:44:11 -0500

    added basic front/back-end dependencies

---
+++ server/index.js
@@ -0,0 +1,35 @@
+/****** LOCAL SERVER ******/
+require('dotenv').config();
+const express = require('express');
+const { json } = require('body-parser');
+const cors = require('cors');
+const massive = require('massive');
+const session = require('express-session');
+
+// Sets up express server
+const app = express();
+
+app.use(json());
+app.use(cors());
+
+// Allows local server to utilize SQL commands within db folder
+massive(process.env.CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set("db", dbInstance);
+}).catch(err => console.log(err));
+
+// Allows application to be utilized by multiple users
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 7
+    }
+  })
+);
+
+// Runs the server on localhost:3001
+const port = process.env.PORT || 3001;
+app.listen(port, () => { console.log(`Listening @ port: ${ port }`) });
