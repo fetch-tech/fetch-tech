3bfc03309b83ec2715bbc9d1b3346567aede20dd
gv4383 <gregory.vargas18@gmail.com>
2018-08-06 14:07:24 -0500

    created basic articles reducer

---
+++ src/redux/ducks/articlesReducer.js
@@ -0,0 +1,42 @@
+/****** INITIAL STATE ******/
+const initialState = {
+  isLoading: false,
+  articles: []
+}
+
+/****** ACTION TYPES ******/
+const GET_ARTICLES = 'GET_ARTICLES';
+
+/****** ACTION CREATORS ******/
+export function getarticles() {
+  return {
+    type: GET_ARTICLES,
+    payload: axios.get('/api/articles')
+  }
+}
+
+/****** REDUCER ******/
+export default function articlesReducer(state = initialState, action) {
+  switch(action.type) {
+    // GET ARTICLES
+    case 'GET_ARTICLES_PENDING':
+      return {
+        ...state,
+        isLoading: true
+    };
+    case 'GET_ARTICLES_FULFILLED':
+      return {
+        ...state,
+        articles: action.payload.data
+    };
+    case 'GET_ARTICLES_REJECTED':
+      return {
+        ...state,
+        error: action.payload
+    };
+
+    // DEFAULT
+    default:
+      return state;
+  }
+}
