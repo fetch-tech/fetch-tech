59ff7c9d2efbb7abfe47a9ba0b11178ceaae9a40
Power-Of-Code <power-of-code@Deans-MacBook-Air.local>
2018-08-07 11:06:18 -0500

    post-article

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import "./components/Post_Article/post_article.css";
 
 import Post_Article from "./components/Post_Article/Post_Article";
+import SearchBar from "./components/Post_Article/SearchBar";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Post_Article />
+        <SearchBar />
       </div>
     );
   }
