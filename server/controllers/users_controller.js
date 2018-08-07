6a72faa9a94b7ba054b17908e49254ded70b72f4
gv4383 <gregory.vargas18@gmail.com>
2018-08-06 23:11:16 -0500

    added comments for getUser method in users_controller.js

--- server/controllers/users_controller.js
+++ server/controllers/users_controller.js
@@ -1,6 +1,8 @@
 /* user_controller contains all methods that deal with the user's information within the application */
 
+// Retrieves specific user's data from database
 const getUser = (req, res, next) => {
+  // user_id from params
   const { id } = req.params;
 
   const db = req.app.get('db');
