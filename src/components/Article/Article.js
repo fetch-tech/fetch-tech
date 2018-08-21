f3de032ea89513cd1ffaf74b712cd42a3673f54c
gv4383 <gregory.vargas18@gmail.com>
2018-08-21 09:27:57 -0500

    Added conditional rendering to Article component
    Added button at bottom of article card that toggles the comment section on/off

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -15,61 +15,129 @@ class Article extends Component {
     };
   }
 
+  toggleHandler = () => {
+    this.setState({
+      toggleComments: !this.state.toggleComments
+    });
+  };
+
   render() {
     const { article, e, noImage } = this.props;
 
-    return (
-      <div className="wrapper" key={e}>
-        <div className="card-sidebar">
-          <div className="card-comment">
-            <Card hoverable className="card-body">
-              <div className="articleWrapper">
-                <img
-                  className="urlToImage"
-                  src={article.urlToImage ? article.urlToImage : noImage}
-                  alt=""
-                />
-                <div className="aricle-center">
-                  <h2>{article.title}</h2>
-                  <div className="article-description">
-                    {article.description}
+    if (this.state.toggleComments) {
+      return (
+        <div className="wrapper" key={e}>
+          <div className="card-sidebar">
+            <div className="card-comment">
+              <Card hoverable className="card-body">
+                <div className="articleWrapper">
+                  <img
+                    className="urlToImage"
+                    src={article.urlToImage ? article.urlToImage : noImage}
+                    alt=""
+                  />
+                  <div className="aricle-center">
+                    <h2>{article.title}</h2>
+                    <div className="article-description">
+                      {article.description}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="card-bottom">
-                <div className="article-bottom">
-                  <div className="source">{article.source.name}</div>
+                <div className="card-bottom">
+                  <div className="article-bottom">
+                    <div className="source">{article.source.name}</div>
+                  </div>
+                  <a target="_blank" href={article.url} target="_blank">
+                    <Button>
+                      Read
+                      <Icon type="right" />
+                    </Button>
+                  </a>
                 </div>
-                <a target="_blank" href={article.url} target="_blank">
-                  <Button>
-                    Read
-                    <Icon type="right" />
+                <div className="showComment">
+                  <Button onClick={this.toggleHandler}>
+                    Hide Comments
+                    <Icon type="up" />
                   </Button>
-                </a>
-              </div>
-            </Card>
-            <Comments
-              article={article}
-              url={article.url}
-              desc={article.description}
-            />
-          </div>
+                </div>
+              </Card>
+              <Comments
+                article={article}
+                url={article.url}
+                desc={article.description}
+              />
+            </div>
 
-          <div className="sidebar">
-            {/* <Claps clapped={article.author} /> */}
-            <div>
-              <Claps article={article} url={article.url} />
-            </div>
-            <div>
-              <Bookmark article={article} url={article.url} />
-            </div>
-            <div>
-              <AddToStories article={article} url={article.url} />
+            <div className="sidebar">
+              {/* <Claps clapped={article.author} /> */}
+              <div>
+                <Claps article={article} url={article.url} />
+              </div>
+              <div>
+                <Bookmark article={article} url={article.url} />
+              </div>
+              <div>
+                <AddToStories article={article} url={article.url} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="wrapper" key={e}>
+          <div className="card-sidebar">
+            <div className="card-comment">
+              <Card hoverable className="card-body">
+                <div className="articleWrapper">
+                  <img
+                    className="urlToImage"
+                    src={article.urlToImage ? article.urlToImage : noImage}
+                    alt=""
+                  />
+                  <div className="aricle-center">
+                    <h2>{article.title}</h2>
+                    <div className="article-description">
+                      {article.description}
+                    </div>
+                  </div>
+                </div>
+                <div className="card-bottom">
+                  <div className="article-bottom">
+                    <div className="source">{article.source.name}</div>
+                  </div>
+                  <a target="_blank" href={article.url} target="_blank">
+                    <Button>
+                      Read
+                      <Icon type="right" />
+                    </Button>
+                  </a>
+                </div>
+                <div className="showComment">
+                  <Button onClick={this.toggleHandler}>
+                    Show Comments
+                    <Icon type="down" />
+                  </Button>
+                </div>
+              </Card>
+            </div>
+
+            <div className="sidebar">
+              {/* <Claps clapped={article.author} /> */}
+              <div>
+                <Claps article={article} url={article.url} />
+              </div>
+              <div>
+                <Bookmark article={article} url={article.url} />
+              </div>
+              <div>
+                <AddToStories article={article} url={article.url} />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
