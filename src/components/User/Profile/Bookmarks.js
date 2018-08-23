02f3db2b3129b75d0a690d0b9293ad3a6aff6cea
gv4383 <gregory.vargas18@gmail.com>
2018-08-22 22:23:51 -0500

    restyled bookmarks component

--- src/components/User/Profile/Bookmarks.js
+++ src/components/User/Profile/Bookmarks.js
@@ -24,52 +24,44 @@ export default class Bookmarks extends Component {
     const { bookmarks } = this.state;
     const bookmarksDisplay = bookmarks.map((article, i) => {
       return (
-        <div className="wrapper" key={i}>
-          <div style={{ display: "flex" }}>
-            <Card
-              hoverable
-              style={{
-                marginLeft: 371,
-                width: 800,
-                marginTop: 5,
-                display: "flex"
-              }}
-            >
-              <div className="articleWrapper">
-                <div
-                  style={{ display: "flex", justifyContent: "space-between" }}
-                >
-                  <div style={{ display: "flex" }}>
-                    <div className="aricle-center">
-                      <h2>{article.title}</h2>
-                      <div style={{ fontSize: "16px" }}>
-                        {article.description}
-                      </div>
-                      <div className="article-bottom">
-                        {/* <div className="source">{article.source.name}</div> */}
-                      </div>
-                      <a target="_blank" href={article.url} target="_blank">
-                        <Button>
-                          Read
-                          <Icon type="right" />
-                        </Button>
-                      </a>
-                    </div>
-                  </div>
-                  <div className="article-sidebar" />
+        <div className="clap-article-wrapper" key={i}>
+          <Card
+            hoverable
+            className="clap-article-card"
+            // style={{
+            //   marginLeft: 371,
+            //   width: 800,
+            //   marginTop: 5,
+            //   display: "flex"
+            // }}
+          >
+            <div className="clap-article-body">
+              <div className="clap-article-top">
+                <h2>{article.title}</h2>
+                <div style={{ fontSize: "16px" }}>{article.description}</div>
+                <div className="article-bottom">
+                  {/* <div className="source">{article.source.name}</div> */}
                 </div>
               </div>
-            </Card>
-          </div>
+              <div className="clap-article-bottom">
+                <a target="_blank" href={article.url} target="_blank">
+                  <Button>
+                    Read
+                    <Icon type="right" />
+                  </Button>
+                </a>
+              </div>
+            </div>
+          </Card>
         </div>
       );
     });
     return (
-      <div>
+      <div className="user-page-claps">
         <div>
           <UserProfile />
         </div>
-        <div className="bookmarks">{bookmarksDisplay}</div>
+        <div className="display-clap-articles">{bookmarksDisplay}</div>
       </div>
     );
   }
